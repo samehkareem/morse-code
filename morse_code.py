17aa2049fab2899633afe18d96a38019e1bc8580
samehkareem <145559847+samehkareem@users.noreply.github.com>
2023-11-28 17:56:39 +0200

    Update morse_code.py

--- morse_code.py
+++ morse_code.py
@@ -29,6 +29,10 @@ morse_code_dict = {
 }
 
 morse_to_alphabet={
+    
+
+    
+    
     '.': 'A',
     '-...': 'B',
     '-.-.': 'C',
@@ -56,3 +60,37 @@ morse_to_alphabet={
     '-.--': 'Y',
     '--..': 'Z',
     ' ': ' '  # Space between words
+def word_to_morse(word):
+    word = word.upper()
+    morse_code = ""
+    for char in word:
+        morse_code += morse_code_dict[char]
+        if char != ' ':
+            morse_code += ' '
+    return morse_code
+word="mohamed"
+print(word_to_morse(word))
+
+def morse_to_word(morse_code):
+    english_word = ""
+    current_letter = ""
+
+    for char in morse_code:
+        if char == '.':
+            current_letter += '.'
+        elif char == '-':
+            current_letter += '-'
+        elif char == ' ':
+            if current_letter in morse_to_alphabet:
+                 english_word+= morse_to_alphabet[current_letter]
+            current_letter = ""
+        else:
+            # Handle unknown characters
+            pass
+
+    if current_letter in morse_to_alphabet:
+        english_word+= morse_to_alphabet[current_letter]
+
+    return english_word
+morse_code="-- --- .... .- -- . -.."
+print(morse_to_word(morse_code))
